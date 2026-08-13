89be8f2283f14fbdce5af7b4d0bc6de261631227
wxs_game <akimchsm@gmail.com>
2026-08-13 22:21:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (tg) {
    БЭКЕНД И АВТОРИЗАЦИЯ
 ========================= */
 
-const API_BASE_URL = "https://cable-coral-ahead.ngrok-free.dev";
+const API_BASE_URL = "https://xezym-5-248-188-249.run.pinggy-free.link";
 
 let currentBalance = 0.00;
 let currentTurnover = 0.00;
