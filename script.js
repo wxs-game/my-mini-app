e48de6808302ad3578b1092ac446ef22fe10e7c2
wxs_game <akimchsm@gmail.com>
2026-08-13 22:35:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if (tg) {
    БЭКЕНД И АВТОРИЗАЦИЯ
 ========================= */
 
-const API_BASE_URL = "https://dghbc-5-248-188-249.free.pinggy.net";
+const API_BASE_URL = "https://oyexn-178-137-18-3.run.pinggy-free.link";
 
 let currentBalance = 0.00;
 let currentTurnover = 0.00;
